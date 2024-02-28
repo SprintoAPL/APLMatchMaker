c61f68c132f9f7cf112d38fd11aac2294672b761
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2024-02-28 15:55:50 +0100

    Add course Dto, service.

---
+++ APLMatchMaker/Client/Services/CourseService.cs
@@ -0,0 +1,45 @@
+﻿using APLMatchMaker.Shared.DTOs;
+
+namespace APLMatchMaker.Client.Services
+{
+    public class CourseService : ICourseService
+    {
+        public void AddCourseAsync(CourseDto course)
+        {
+            Random rnd = new Random();
+            course.Id = rnd.Next(100000);
+            Courses.Add(course);
+        }
+
+        public void DeleteCourseAsync(int id)
+        {
+            var courseToDelete = Courses.FirstOrDefault(c => c.Id == id);
+            if(courseToDelete != null)
+            {
+                courseToDelete.Delete();
+            }
+        }
+
+        public List<CourseDto> GetAllCoursesAsync()
+        {
+            return Courses;
+        }
+
+        public CourseDto GetCourseByIdAsync(int id)
+        {
+            return Courses.FirstOrDefault(c => c.Id == Id);  
+        }
+
+        public void UpdateCourseAsync(CourseDto updatedcourse)
+        {
+            var Course = Courses.FirstOrDefault(c => c.Id == updatedcourse.Id);
+            if(Course != null)
+            {
+                Course.Name = updatedcourse.Name;
+                Course.Description = updatedcourse.Description;
+                Course.StartDate = updatedcourse.StartDate;
+                Course.EndDate = updatedcourse.EndDate;
+            }
+        }
+    }
+}
---
+++ APLMatchMaker/Client/Services/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using APLMatchMaker.Shared.DTOs;
+
+namespace APLMatchMaker.Client.Services
+{
+    public interface ICourseService
+    {
+        List<CourseDto> GetAllCoursesAsync();
+        CourseDto GetCourseByIdAsync(int id);
+        void AddCourseAsync(CourseDto course);
+        void DeleteCourseAsync(int id);
+        void UpdateCourseAsync(CourseDto updatedcourse);
+    }
+}
