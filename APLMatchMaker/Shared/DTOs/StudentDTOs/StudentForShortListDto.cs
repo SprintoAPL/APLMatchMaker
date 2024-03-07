129a44070d5f5837b0214974d3033deb8e534d43
SushmaSrinivasan <sushmasrinivasan04@gmail.com>
2024-03-07 11:19:38 +0100

    Created a short list student DTO and added a nav link for students

---
+++ APLMatchMaker/Shared/DTOs/StudentDTOs/StudentForShortListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APLMatchMaker.Shared.DTOs.StudentDTOs
+{
+    public class StudentForShortListDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string StudentSocSecNo { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        public int PhoneNumber { get; set; }
+
+    }
+}
