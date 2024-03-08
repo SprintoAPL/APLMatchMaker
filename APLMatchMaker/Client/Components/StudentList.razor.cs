75b9fd738a430038b906b71c7b23019bf5e0b9cf
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2024-03-08 16:54:17 +0100

    Added razor pages(components) to display list of students

--- APLMatchMaker/Client/Components/StudentList.razor.cs
+++ APLMatchMaker/Client/Components/StudentList.razor.cs
@@ -1,6 +1,12 @@
-﻿namespace APLMatchMaker.Client.Components
+﻿using Microsoft.AspNetCore.Components;
+using APLMatchMaker.Shared.DTOs.StudentsDTOs;
+using APLMatchMaker.Shared.DTOs.CoursesDTOs;
+
+namespace APLMatchMaker.Client.Components
 {
-    public partial class StudentList
+    public partial class StudentList : ComponentBase
     {
+        [Parameter]
+        public  IEnumerable<StudentForListDTO> Students { get; set; } = new List<StudentForListDTO>();
     }
 }
