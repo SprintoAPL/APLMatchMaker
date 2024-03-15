7c3cdb785ca6e7ab7744d11808aec15d7262fec7
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2024-03-15 09:05:53 +0100

    Added Create student component (razor page to Add new student.

---
+++ APLMatchMaker/Client/Pages/StudentCreatePage.razor.cs
@@ -0,0 +1,35 @@
+﻿using APLMatchMaker.Shared.DTOs.StudentsDTOs;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace APLMatchMaker.Client.Pages
+{
+    public partial class StudentCreatePage : ComponentBase
+    {
+        private StudentForCreateDTO student = new StudentForCreateDTO(); // Initialize with default values or null
+        private string? errorMessage;
+
+        [Inject]
+        private HttpClient? Http { get; set; }
+
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+
+        private async Task CreateStudent(StudentForCreateDTO student)
+        {
+            try
+            {
+                var response = await Http!.PostAsJsonAsync("api/student", student);
+                response.EnsureSuccessStatusCode();
+                NavigationManager!.NavigateTo("/student-list");
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
+}
