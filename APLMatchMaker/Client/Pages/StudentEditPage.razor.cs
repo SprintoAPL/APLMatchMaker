7ee154093a849710e43299d060cf1f88b033753d
SushmaSrinivasan <sushmasrinivasan04@gmail.com>
2024-03-21 13:54:41 +0100

    Fixed StudentDetailDTO for fullName and added a edit page razor component with its behind class

---
+++ APLMatchMaker/Client/Pages/StudentEditPage.razor.cs
@@ -0,0 +1,62 @@
+﻿using APLMatchMaker.Shared.DTOs.StudentsDTOs;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace APLMatchMaker.Client.Pages
+{
+    public partial class StudentEditPage
+    {
+        private StudentForUpdateDTO editStudent = new StudentForUpdateDTO();
+
+        private string? ErrorMessage;
+
+        [Inject]
+        private HttpClient? Http { get; set; }
+
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+
+        [Parameter]
+        public string? Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                editStudent = await Http!.GetFromJsonAsync<StudentForUpdateDTO>($"api/student/{Id}");
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+        public async Task HandleValidSubmit()
+        {
+            try
+            {
+                var response = await Http!.PatchAsJsonAsync($"api/student/{Id}", editStudent);
+                if (!response.IsSuccessStatusCode)
+                {
+                    NavigationManager!.NavigateTo("/studentdetails");
+                }
+                else
+                {
+                    ErrorMessage = "Kunde inte redigera eleven!";
+                }
+            }
+            catch(Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+            
+        }
+
+        private void Cancel()
+        {
+            NavigationManager?.NavigateTo("/ListOfStudents");
+        }
+
+
+    }
+}
