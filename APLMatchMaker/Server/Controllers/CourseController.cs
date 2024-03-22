de5898318eeb36a4f2d0d00b221b731f446f447b
Sammy Wong <sammywong528@outlook.com>
2024-03-22 14:31:19 +0100

    Solved temporary the problem with adding course. Will rewrite the controller for correcting the error.

--- APLMatchMaker/Server/Controllers/CourseController.cs
+++ APLMatchMaker/Server/Controllers/CourseController.cs
@@ -64,7 +64,8 @@ namespace APLMatchMaker.Server.Controllers
 
                 await _courseService.AddCourseAsync(courseDto);
 
-                return CreatedAtAction("GetCourseByIdAsync", new { id = courseDto.Id }, courseDto);
+                //return CreatedAtAction("GetCourseByIdAsync", new { id = courseDto.Id }, courseDto);
+                return Ok($"{courseDto.Name} is added.");
 
             }
             catch (Exception ex)
@@ -84,7 +85,7 @@ namespace APLMatchMaker.Server.Controllers
                 }
 
                 await _courseService.UpdateCourseAsync(courseDto);
-                return Ok();
+                return Ok($"{courseDto.Name} is updated.");
             }
             catch (Exception ex)
             {
@@ -98,7 +99,7 @@ namespace APLMatchMaker.Server.Controllers
             try
             {
                 await _courseService.DeleteCourseAsync(id);
-                return Ok();
+                return Ok("The course has been removed.");
             }
             catch (Exception ex)
             {
