aa3aa2114b1978527d1f303d2468f25e24d93c2c
Abbe-Mohamud <mohamud99@gmail.com>
2024-03-25 10:17:40 +0100

    CompanyDTOS-And-ListOverview

---
+++ APLMatchMaker/Shared/DTOs/CompanyDTOs/CompanyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APLMatchMaker.Shared.DTOs.CompanyDTOs
+{
+    public class CompanyDTO
+    {
+        public int Companyid { get; set; }
+        public string CompanyName { get; set; } = null!;
+        public string OrgNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+    }
+}
