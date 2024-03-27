b0a825eaf7f0c214b43f39fad0f1d35c9a3059df
Sammy Wong <sammywong528@outlook.com>
2024-03-27 07:12:08 +0100

    Create CourseForEditDto, create EditCourse page.

---
+++ APLMatchMaker/Shared/DTOs/CoursesDTOs/CourseForEditDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APLMatchMaker.Shared.DTOs.CoursesDTOs
+{
+    public class CourseForEditDto
+    {
+        [Required(ErrorMessage = "Du måste ange ett namn för kursen.")]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(100, ErrorMessage = "Beskrivningen kan enbart innehålla 100 tecken.")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Du måste ange ett startdatum.")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Du måste ange ett slutdatum.")]
+        public DateTime EndDate { get; set; }
+    }
+}
