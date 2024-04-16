a90d777bcb193bf9acf3144d425641667d9dd26a
Sammy Wong <sammywong528@outlook.com>
2024-04-16 08:34:00 +0200

    Implement frontend for search bar but still not working.

--- APLMatchMaker/Client/Components/SearchBarStudent.razor.cs
+++ APLMatchMaker/Client/Components/SearchBarStudent.razor.cs
@@ -1,11 +1,24 @@
 ﻿using APLMatchMaker.Shared.DTOs.StudentsDTOs;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace APLMatchMaker.Client.Components
 {
     public partial class SearchBarStudent
     {
-        private string SearchText = "";
-
-        
+        [Inject]
+        public HttpClient? Http {  get; set; }
+        private string SearchQuery = "";
+        private List<StudentForListDTO> SearchResult = new List<StudentForListDTO>();
+           
+        public async Task SearchStudents(string searchText)
+        {
+            SearchQuery = searchText;
+            var response = await Http.GetAsync($"api/student?SearchQuery={searchText}");
+            if (response.IsSuccessStatusCode)
+            {
+                SearchResult = await response.Content.ReadFromJsonAsync<List<StudentForListDTO>>();
+            }
+        }
     }
 }
