8619890d32763fe99e7b7e1ac8a78e2804cc8c54
Sammy Wong <sammywong528@outlook.com>
2024-05-08 11:06:32 +0200

    Left posts with no data empty.

--- APLMatchMaker/Client/Helpers/Status.cs
+++ APLMatchMaker/Client/Helpers/Status.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace APLMatchMaker.Shared.Dictionaries
+namespace APLMatchMaker.Client.Helpers
 {
     public static class Status
     {
